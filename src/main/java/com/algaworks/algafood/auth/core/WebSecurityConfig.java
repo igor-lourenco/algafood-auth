ff3821d670f99569ec8e6af7b565cdefabb5b2ba
igor-lourenco <igor.lourencosantos@gmail.com>
2025-04-13 16:31:26 -0300

    Removendo a versão depreciada do spring authorization server e atualizando versão do pom xml para 2.7.5

--- src/main/java/com/algaworks/algafood/auth/core/WebSecurityConfig.java
+++ src/main/java/com/algaworks/algafood/auth/core/WebSecurityConfig.java
@@ -1,51 +1,9 @@
 package com.algaworks.algafood.auth.core;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+///**  Essa classe configura a segurança da aplicação web em geral, definindo usuários, codificando senhas
+//  com BCrypt e fornecendo um gerenciador de autenticação.  */
+//@Configuration
+//@EnableWebSecurity  //  Habilita a configuração de segurança da web no Spring Security.
+public class WebSecurityConfig {
 
-/**  Essa classe configura a segurança da aplicação web em geral, definindo usuários, codificando senhas
-  com BCrypt e fornecendo um gerenciador de autenticação.  */
-@Configuration
-@EnableWebSecurity  //  Habilita a configuração de segurança da web no Spring Security.
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-
-    @Bean // Define um bean de PasswordEncoder que usa BCryptPasswordEncoder para codificar senhas.
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-
-
-    @Bean
-    @Override //  Define um bean de AuthenticationManager que usa a implementação padrão fornecida pela superclasse.
-    protected AuthenticationManager authenticationManager() throws Exception{
-        return super.authenticationManager();
-    }
-
-//    Implementação para usar os dados do usuário em memória. Obs: foi implementado para pegar usuario final no banco de dados(UsuarioModel)
-//    @Override // Configura a autenticação do usuário final (Resource Owner)
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//
-//        auth.inMemoryAuthentication() // Indica que a autenticação será configurada em memória. Os dados dos usuários são definidos diretamente no código e mantidos apenas enquanto a aplicação está em execução.
-//                .withUser("igor")
-//                .password(passwordEncoder().encode("123"))
-//                .roles("ADMIN")
-//            .and()
-//                .withUser("joao")
-//                .password(passwordEncoder().encode("123"))
-//                .roles("ADMIN");
-//
-//    }
-
-//    Implementação para usar os dados do usuário em memória
-//    @Bean
-//    @Override
-//    protected UserDetailsService userDetailsService() {
-//        return super.userDetailsService();
-//    }
 }
--- src/main/java/com/algaworks/algafood/auth/services/enhancers/JwtCustomClaimsTokenEnhancer.java
+++ src/main/java/com/algaworks/algafood/auth/services/enhancers/JwtCustomClaimsTokenEnhancer.java
@@ -1,44 +1,12 @@
 package com.algaworks.algafood.auth.services.enhancers;
+//
+///** Essa classe permite adicionar nossas informações personalizadas ao token, como claims personalizados.
+// *
+// * TokenEnhancer - essa interface é usada pelo Spring Security para personalizar um token de acesso antes de ser
+// *   criptografado e assinado(Stateless), ou antes, de ele ser armazenado pelo servidor de autorização(StateFul)
+// *
+// * Claims - São as propriedades do payload JSON do JWT, por exemplo: 'user_name', 'client_id', 'exp'
+// * */
+public class JwtCustomClaimsTokenEnhancer {
 
-import com.algaworks.algafood.auth.services.AuthUser;
-import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
-import org.springframework.security.oauth2.common.OAuth2AccessToken;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.security.oauth2.provider.token.TokenEnhancer;
-
-import java.util.HashMap;
-
-/** Essa classe permite adicionar nossas informações personalizadas ao token, como claims personalizados.
- *
- * TokenEnhancer - essa interface é usada pelo Spring Security para personalizar um token de acesso antes de ser
- *   criptografado e assinado(Stateless), ou antes, de ele ser armazenado pelo servidor de autorização(StateFul)
- *
- * Claims - São as propriedades do payload JSON do JWT, por exemplo: 'user_name', 'client_id', 'exp'
- * */
-public class JwtCustomClaimsTokenEnhancer implements TokenEnhancer {
-
-
-    @Override //Este método permite adicionar informações adicionais ao token, como claims personalizados, que podem ser úteis para autenticação e autorização
-    public OAuth2AccessToken enhance(OAuth2AccessToken oAuth2AccessToken, OAuth2Authentication oAuth2Authentication) {
-//   OAuth2AccessToken - representa o token antes de ser criptografado, assinado e emitido
-//   OAuth2Authentication - representa as informações da autenticação e os dados do cliente autenticado
-
-//      Verifica se é uma instância da nossa classe AuthUser para evitar exception porque nem todos os fluxos fazem a autenticação do usuario final, por exemplo o Client Credentials(client_credentials)
-        if(oAuth2Authentication.getPrincipal() instanceof AuthUser) {
-
-            AuthUser authUser = (AuthUser) oAuth2Authentication.getPrincipal(); // contém informações detalhadas sobre o usuário
-
-            var info = new HashMap<String, Object>();
-            info.put("nome_completo", authUser.getFullName());
-            info.put("usuario_id", authUser.getUserId());
-
-
-            var accessToken = (DefaultOAuth2AccessToken) oAuth2AccessToken; // cast para a classe que implementa a interface OAuth2AccessToken
-
-            accessToken.setAdditionalInformation(info); // adiciona nossas informações adicionais(claims) ao token
-        }
-
-
-        return oAuth2AccessToken;
-    }
 }
